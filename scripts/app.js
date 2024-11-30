603f9384b1d57108115898299f596b376a24cb42
widenhrzns <widenhrzns@gmail.com>
2024-12-01 01:22:19 +0300

    load N save Data

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let habits = [];
+const HABIT_KEY = "HABIT_KEY";
+
+/* utils */
+function loadData() {
+  const habitsString = localStorage.getItem(HABIT_KEY);
+  const habitsArray = JSON.parse(habitsString);
+  if (Array.isArray(habitsArray)) {
+    habits = habitsArray;
+  }
+}
+
+function saveData() {
+  localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+(() => {
+  loadData();
+})();
