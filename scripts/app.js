63ceda4617d396c35cadfbd84a409dfaf74fa138
widenhrzns <widenhrzns@gmail.com>
2024-12-05 20:39:36 +0300

    Добавление новой привычки

--- scripts/app.js
+++ scripts/app.js
@@ -35,6 +35,44 @@ function saveData() {
   localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
 }
 
+function togglePopup() {
+  const isClosed = page.popup.index.classList.contains("cover_hidden");
+  if (isClosed) {
+    page.popup.index.classList.remove("cover_hidden");
+  } else {
+    page.popup.index.classList.add("cover_hidden");
+  }
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = "";
+  }
+}
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove("error");
+    if (!fieldValue) {
+      form[field].classList.add("error");
+      return;
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
+}
+
 /* render */
 function rerenderMenu(activeHabit) {
   for (const habit of habits) {
@@ -103,25 +141,21 @@ function rerender(activeHabitId) {
 
 /* work with days */
 function addDay(event) {
-  const form = event.target;
   event.preventDefault();
-  const data = new FormData(form);
-  const comment = data.get("comment");
-  form["comment"].classList.remove("error");
-  if (!comment) {
-    form["comment"].classList.add("error");
+  const data = validateAndGetFormData(event.target, ["comment"]);
+  if (!data) {
     return;
   }
   habits = habits.map((habit) => {
     if (habit.id === globalActiveHabitId) {
       return {
         ...habit,
-        days: habit.days.concat({ comment }),
+        days: habit.days.concat({ comment: data.comment }),
       };
     }
     return habit;
   });
-  form["comment"].value = "";
+  resetForm(event.target, ["comment"]);
   rerender(globalActiveHabitId);
   saveData();
 }
@@ -141,22 +175,35 @@ function removeDay(index) {
   saveData();
 }
 
-/* pop-up opening */
-function togglePopup() {
-  const isClosed = page.popup.index.classList.contains("cover_hidden");
-  if (isClosed) {
-    page.popup.index.classList.remove("cover_hidden");
-  } else {
-    page.popup.index.classList.add("cover_hidden");
-  }
-}
-
 /* working with habits */
 function setIcon(context, icon) {
   page.popup.iconField.value = icon;
   const activeIcon = document.querySelector(".icon.icon_active");
   activeIcon.classList.remove("icon_active");
   context.classList.add("icon_active");
+}
+
+function addHabit(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ["name", "icon", "target"]);
+  if (!data) {
+    return;
+  }
+  const maxId = habits.reduce(
+    (acc, habit) => (acc > habit.id ? acc : habit.id),
+    0
+  );
+  habits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: [],
+  });
+  resetForm(event.target, ["name", "target"]);
+  togglePopup();
+  saveData();
+  rerender(maxId + 1);
 }
 
 /* init */
